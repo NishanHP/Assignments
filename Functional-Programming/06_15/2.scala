a4b7cb27211bf3949c8bb9af775438099a554f81
Hiruna Pramuditha <129140129+NishanHP@users.noreply.github.com>
2024-06-23 11:01:11 +0530

    Add files via upload

---
+++ Functional-Programming/06_15/2.scala
@@ -0,0 +1,28 @@
+object main extends App {
+  import scala.math.Numeric.Implicits.infixNumericOps;
+
+  var a: Int = 2;
+  var b: Int = 3;
+  var c: Int = 4;
+  var d: Int = 5;
+  var k: Float = 4.3;
+
+  // a)
+  b -= 1;
+  d -= 1;
+  println(b * a + c * d)
+
+  // b)
+  a += 1;
+  println(a);
+
+  // c) cannot be run since g isn't declared
+  // d)
+  c += 1;
+  println(c);
+
+  // e)
+  a += 1;
+  c = c * a;
+  println(c);
+}
